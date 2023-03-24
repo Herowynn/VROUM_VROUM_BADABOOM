7475e08cec35c47c83a8bf1dc7d0825999877842
Jambon <armand.deffrennes@student.junia.com>
2023-03-24 10:32:47 +0100

    Options Menu Finished

---
+++ Assets/Scripts/Menu/Options.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    [Header("Instance")]
+    public AudioMixer AudioMixer;
+    public TMP_Dropdown ResolutionDropdown;
+    private Resolution[] _resolutions;
+    
+    private void Start()
+    {
+        SetQuality(0);
+        CreateResolutionItems();
+    }
+
+    private void CreateResolutionItems()
+    {
+        _resolutions = Screen.resolutions;
+        ResolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0, cpt = 0;
+        
+        foreach (var resolution in _resolutions)
+        {
+            options.Add(resolution.width + " x " + resolution.height);
+
+            if (resolution.width == Screen.currentResolution.width &&
+                resolution.height == Screen.currentResolution.height)
+                currentResolutionIndex = cpt;
+
+            cpt++;
+        }
+        
+        ResolutionDropdown.AddOptions(options);
+        ResolutionDropdown.value = currentResolutionIndex;
+        ResolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetVolume(float volume) 
+    {
+        AudioMixer.SetFloat("MainVolume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
