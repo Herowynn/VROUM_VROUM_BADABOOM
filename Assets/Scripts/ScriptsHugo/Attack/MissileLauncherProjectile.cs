e81f0f97d82cb6c94ad4cb2c6ef09fef8fc3dc0f
Hugo Vanderhaegen <hugo.vanderhaegen@student.junia.com>
2023-03-30 11:44:10 +0200

    Missile almost finished

--- Assets/Scripts/ScriptsHugo/Attack/MissileLauncherProjectile.cs
+++ Assets/Scripts/ScriptsHugo/Attack/MissileLauncherProjectile.cs
@@ -6,13 +6,29 @@ public class MissileLauncherProjectile : MonoBehaviour
 {
     Rigidbody _targetRB;
     Rigidbody _projectileRB;
-   
+    public int FollowSpeed;
+    Vector3 direction;
+
+    private void Start()
+    {
+          _projectileRB= GetComponent<Rigidbody>();
+    }
     public void Init(Rigidbody rb)
     {
-       
+        _targetRB= rb;
+      
+    }
+    public void InitNoTarget(Vector3 dir)
+    {
+        direction = dir;
     }
     void Update()
     {
-        
+        if (_targetRB == null) _projectileRB.AddForce(direction * FollowSpeed, ForceMode.Acceleration); ;
+        if (_targetRB != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, _targetRB.gameObject.transform.position, FollowSpeed * Time.deltaTime);
+            transform.LookAt(_targetRB.transform);
+        }
     }
 }
