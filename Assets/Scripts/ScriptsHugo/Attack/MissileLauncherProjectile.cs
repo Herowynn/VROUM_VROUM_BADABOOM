89c93f1375e2c3bde187cbb15bf784275d5214fa
Kovanderhaegen <hugo.vanderhaegen@student.junia.com>
2023-04-03 14:24:29 +0200

    fix

--- Assets/Scripts/ScriptsHugo/Attack/MissileLauncherProjectile.cs
+++ Assets/Scripts/ScriptsHugo/Attack/MissileLauncherProjectile.cs
@@ -8,31 +8,39 @@ public class MissileLauncherProjectile : MonoBehaviour
     Rigidbody _projectileRB;
     public int FollowSpeed;
     Vector3 direction;
-
+    public float _clock;
+    bool _moved = false;
+    private int _stageOne = 1, _stageTwo = 2;
+    private int _stageOneSpeed = 500;
+    private int _stageTwoSpeed = 50;
     private void Start()
     {
         _projectileRB = GetComponent<Rigidbody>();
     }
-    public void Init(Rigidbody rb)
+    public void Init(Rigidbody rb, Vector3 dir)
     {
         _targetRB = rb;
         direction = dir;
-        
+
     }
 
     void Update()
     {
-        if (_targetRB == null) _projectileRB.AddForce(direction * FollowSpeed, ForceMode.Acceleration); ;
-        if (_targetRB != null)
+        if (_clock < _stageOne && _moved == false)
         {
-            _projectileRB.AddForce(direction * 500, ForceMode.Acceleration);
+            _projectileRB.AddForce(direction * _stageOneSpeed, ForceMode.Acceleration);
             _moved = true;
         }
-        if (_targetRB != null && _clock > 1)
+        if (_targetRB != null && _clock > _stageOne && _clock < _stageTwo)
         {
             _projectileRB.velocity = new Vector3(0, 0, 0);
             transform.position = Vector3.Lerp(transform.position, _targetRB.gameObject.transform.position, FollowSpeed * Time.deltaTime);
             transform.LookAt(_targetRB.transform);
         }
+        if(_clock > _stageTwo)
+        {
+            _projectileRB.AddForce(transform.forward * _stageTwoSpeed, ForceMode.Acceleration);
+        }
+        _clock += Time.deltaTime;
     }
 }
