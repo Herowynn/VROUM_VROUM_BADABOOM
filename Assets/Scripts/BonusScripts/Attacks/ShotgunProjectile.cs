525f5cf8e183d53f6a2c53ff0576835cbd436188
Remy <remy.jabbo@gmail.com>
2023-04-13 11:11:18 +0200

    ShotGun sounds add

--- Assets/Scripts/BonusScripts/Attacks/ShotgunProjectile.cs
+++ Assets/Scripts/BonusScripts/Attacks/ShotgunProjectile.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 
 public class ShotgunProjectile : MonoBehaviour
 {
+    public LayerMask GroundLayerMask;
+
     [SerializeField] float _speed;
     Rigidbody ProjectileRB;
     [SerializeField] CarController _carControl;
+    int _ground;
+
+    [Header("Audio")]
+    public AudioClip ShootSound;
+    public AudioClip[] BounceSounds;
+    AudioSource _source;
 
     private void Awake()
     {
         ProjectileRB = GetComponent<Rigidbody>();
+        _source = gameObject.AddComponent<AudioSource>();
+        _ground = (int)Mathf.Log(1f * GroundLayerMask.value, 2f);
     }
     public void Init(Vector3 direction)
     {
         ProjectileRB.AddForce(direction * _speed, ForceMode.Acceleration);
         GetComponent<SphereCollider>().enabled = false;
         Debug.Log(GetComponent<SphereCollider>().enabled);
+        _source.clip = ShootSound;
+        _source.loop = false;
+        _source.Play();
 
         StartCoroutine(WaitBeforeActivateCollider());
     }
@@ -30,6 +43,12 @@ public class ShotgunProjectile : MonoBehaviour
             GetComponent<MeshRenderer>().enabled = false;
             GetComponent<SphereCollider>().enabled = false;
             StartCoroutine(WaitBeforeNormalSpeed());
+        }
+        else if(collision.gameObject.layer != _ground)
+        {
+            _source.clip = BounceSounds[Random.Range(0, BounceSounds.Length)];
+            _source.loop = false;
+            _source.Play();
         }
     }
 
