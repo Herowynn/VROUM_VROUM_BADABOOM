eea02717cff315efbfae08ac149ff7b975d82615
Kovanderhaegen <hugo.vanderhaegen@student.junia.com>
2023-04-17 15:03:39 +0200

    LES CUBES SONT MORTS

--- Assets/Scripts/BonusScripts/Bonus.cs
+++ Assets/Scripts/BonusScripts/Bonus.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+
 public enum BonusType
 {
     Attack,
@@ -11,21 +13,27 @@ public class Bonus : MonoBehaviour
 {
     public BonusType Type;
     public int RndLvl = -1;
-    public List<Material> Materials = new List<Material>();
-    public MeshRenderer BonusMeshRenderer;
+    public List<GameObject> BonusSkin = new List<GameObject>();
+    //public MeshRenderer BonusMeshRenderer;
 
     void Start()
     {
         if (Type == BonusType.Attack) 
         {
-            RndLvl = Random.Range(0, Materials.Count);
-            BonusMeshRenderer.material = Materials[RndLvl];
+            RndLvl = Random.Range(0, BonusSkin.Count);
+            //BonusMeshRenderer.material = BonusSkin[RndLvl];
+            Instantiate(BonusSkin[RndLvl], transform.position, transform.rotation, transform);
         }
         if (Type == BonusType.Boost)
         {
-            RndLvl = Random.Range(0, Materials.Count);
-            BonusMeshRenderer.material = Materials[RndLvl];
+            RndLvl = Random.Range(0, BonusSkin.Count);
+            //BonusMeshRenderer.material = BonusSkin[RndLvl];
+            Instantiate(BonusSkin[RndLvl], transform.position, transform.rotation, transform);
         }
         
+    }
+    private void Update()
+    {
+        transform.Rotate(0.33f*new Vector3(0, 1, 0));
     }
 }
