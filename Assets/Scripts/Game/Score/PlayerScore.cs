e633dd8df78e51cc72ada8ad4abd0ad6095276b6
Jambon <armand.deffrennes@student.junia.com>
2023-04-19 10:33:39 +0200

    Score Comments

--- Assets/Scripts/Game/Score/PlayerScore.cs
+++ Assets/Scripts/Game/Score/PlayerScore.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+/// <summary>
+/// PlayerScore was meant to be used for each player in order to manage their score but couldn't be implemented.
+/// </summary>
+
 public class PlayerScore : MonoBehaviour
 {
     [Header("Info")] 
