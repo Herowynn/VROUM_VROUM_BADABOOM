b955adc69001dfd59b1c185d078b7abb7debf008
Jambon <armand.deffrennes@student.junia.com>
2023-04-19 10:59:08 +0200

    Player Files commented

--- Assets/Scripts/Game/Player/PlayerState.cs
+++ Assets/Scripts/Game/Player/PlayerState.cs
@@ -1,4 +1,9 @@
-﻿public enum PlayerState
+﻿/// <summary>
+/// PlayerState is an enum that is used in every player scripts.
+/// For example it will prevent the player from moving when he is dead.
+/// </summary>
+
+public enum PlayerState
 {
     ALIVE,
     DEAD
