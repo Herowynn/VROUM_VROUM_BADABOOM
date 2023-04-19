e7da6707515debaa47b6366597a336241e7b1221
Alexis SEREUSE <alexis.sereuse@student.junia.com>
2023-04-19 14:56:05 +0200

    One harvester for each map

--- Assets/Scripts/Game/Harvester/HarvesterManager.cs
+++ Assets/Scripts/Game/Harvester/HarvesterManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -5,11 +7,21 @@ using UnityEngine;
 /// </summary>
 public class HarvesterManager : MonoBehaviour
 {
-    [Header("Instances")] 
-    public GameObject HarvesterPrefab;
-    
+    [Header("Instances")]
+    public List<HomemadeStringGameObjectPair> Harvesters = new List<HomemadeStringGameObjectPair>();
+
     [HideInInspector] public GameObject HarvesterGoRef;
     [HideInInspector] public Harvester HarvesterRef;
+
+    private Dictionary<string, GameObject> _harvesters = new Dictionary<string, GameObject>();
+
+    private void Start()
+    {
+        foreach(HomemadeStringGameObjectPair pair in Harvesters)
+        {
+            _harvesters.Add(pair.Key, pair.Value);
+        }
+    }
 
     /// <summary>
     /// This method instantiates the harvester prefab, sets its scale, its rotation and calls the InitiateNodesToFollow 
@@ -17,13 +29,26 @@ public class HarvesterManager : MonoBehaviour
     /// </summary>
     public void InstantiateHarvester()
     {
-        HarvesterGoRef = Instantiate(HarvesterPrefab, GameManager.Instance.MapManager.CurrentMap.HarvesterStartPosition.position, Quaternion.identity, transform);
+        HarvesterGoRef = Instantiate(_harvesters[GameManager.Instance.MapManager.CurrentMap.name], GameManager.Instance.MapManager.CurrentMap.HarvesterStartPosition.position, Quaternion.identity, transform);
 
         //Scale fix
-        HarvesterGoRef.transform.localScale = GameManager.Instance.MapManager.CurrentMap.gameObject.transform.localScale;
+        /*        HarvesterGoRef.transform.localScale = GameManager.Instance.MapManager.CurrentMap.gameObject.transform.localScale;*/
         HarvesterGoRef.transform.rotation = GameManager.Instance.MapManager.CurrentMap.HarvesterStartPosition.rotation;
         
         HarvesterRef = HarvesterGoRef.GetComponent<Harvester>();
         HarvesterRef.InitiateNodesToFollow(GameManager.Instance.MapManager.CurrentMap.HarvesterNodes);
+    }
+}
+
+[Serializable]
+public class HomemadeStringGameObjectPair
+{
+    public string Key;
+    public GameObject Value;
+
+    public HomemadeStringGameObjectPair(string key, GameObject value)
+    {
+        Key = key;
+        Value = value;
     }
 }
