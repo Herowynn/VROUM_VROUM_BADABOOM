151b78e5051445b7702a76a64e9d26480d214316
Jambon <armand.deffrennes@student.junia.com>
2023-04-19 09:47:28 +0200

    GameManager and GameState

--- Assets/Scripts/Game/GameState.cs
+++ Assets/Scripts/Game/GameState.cs
@@ -1,4 +1,9 @@
-﻿public enum GameState
+﻿/// <summary>
+/// GameState is an enum that is used referenced in GameManager script
+/// and used in most of the script to check if the game is currently running or not
+/// </summary>
+
+public enum GameState
 {
     RACING,
     NOT_RACING,
