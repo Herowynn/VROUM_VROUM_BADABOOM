258661f5025edc1dd2782472fb60ec150489e32c
Alexis SEREUSE <alexis.sereuse@student.junia.com>
2023-04-19 15:42:05 +0200

    One harvester for each map v2

--- Assets/Scripts/Game/Car/CarController.cs
+++ Assets/Scripts/Game/Car/CarController.cs
@@ -129,7 +129,6 @@ public class CarController : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(SlowFactor);
         _distArrowRayPoint = Arrow.transform.position - RayPoint.position;
 
         if (GameManager.Instance.GameState == GameState.RACING && PlayerState == PlayerState.ALIVE && _movementInput != Vector2.zero)
